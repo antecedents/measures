90883ca3fdcb341251fd2ef69e8722b6d2d1771a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-15 21:21:52 +0000

    Update src/algorithms/drawing.py

--- src/algorithms/drawing.py
+++ src/algorithms/drawing.py
@@ -5,4 +5,17 @@ import numpy as np
 class Drawing:
 
     def __init__(self):
-        pass
+
+        self.__fields = ['milliseconds', 'observation', 'trend', 'seasonal', 'residue', 'weight']
+
+    def __get_instances(self, blob: pd.DataFrame) -> dict:
+
+        instances = blob[self.__fields].to_dict(orient='split')
+
+        return instances
+
+    def exc(self, data: pd.DataFrame, health_board_code: str, hospital_code):
+
+        instances: dict = self.__get_instances(blob=data)
+        instances['health_board_code'] = health_board_code
+        instances['hospital_code'] = hospital_code
