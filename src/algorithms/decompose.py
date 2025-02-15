1bb49e95a7e3ffa0e45dbb1fd09b8b92df1ede92
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-15 17:46:56 +0000

    Update src/algorithms/decompose.py

--- src/algorithms/decompose.py
+++ src/algorithms/decompose.py
@@ -1,9 +1,11 @@
-
+import logging
 import pandas as pd
 import numpy as np
 import dask
 
-import statsmodels.tsa.seasonal as stsl
+import statsmodels.tsa.seasonal as stl
+
+import config
 
 class Decompose:
 
@@ -11,6 +13,22 @@ class Decompose:
 
         self.__data = data
 
+        self.__configurations = config.Config()
+
+    @dask.delayed
+    def __slice(self, code: str) -> pd.DataFrame:
+
+        return self.__data.copy()[self.__data['hospital_code'] == code, :]
+
+    @dask.delayed
+    def __decompose(self, blob: pd.DataFrame):
+
+        parts = stl.seasonal_decompose(
+            x=blob['n_attendances'], model='additive', period=self.__configurations.seasons)
+
     def exc(self):
 
         codes: np.ndarray = self.__data['hospital_code'].unique()
+
+        for code in codes:
+            logging.info(code)
