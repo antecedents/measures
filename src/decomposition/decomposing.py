b89d9a92245b5980e83a63cd4a6ca5293c2a8769
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-15 22:54:35 +0000

    Update src/decomposition/decomposing.py: In Progress

--- src/decomposition/decomposing.py
+++ src/decomposition/decomposing.py
@@ -6,6 +6,7 @@ import pandas as pd
 import statsmodels.tsa.seasonal as stl
 
 import config
+import src.decomposition.structuring
 
 
 class Decomposing:
@@ -19,6 +20,7 @@ class Decomposing:
         self.__data = data
 
         self.__configurations = config.Config()
+        self.__structuring = src.decomposition.structuring.Structuring()
 
     @dask.delayed
     def __get_data(self, code: str) -> pd.DataFrame:
@@ -26,12 +28,17 @@ class Decomposing:
         return self.__data.copy()[self.__data['hospital_code'] == code, :]
 
     @dask.delayed
-    def __decompose(self, blob: pd.DataFrame) -> stl.DecomposeResult:
+    def __decompose(self, frame: pd.DataFrame) -> stl.DecomposeResult:
 
         parts = stl.seasonal_decompose(
-            x=blob['n_attendances'], model='additive', period=self.__configurations.seasons)
+            x=frame['n_attendances'], model='additive', period=self.__configurations.seasons)
 
         return parts
+
+    @dask.delayed
+    def __exc_structuring(self, parts: stl.DecomposeResult) -> pd.DataFrame:
+
+        return self.__structuring.exc(parts=parts)
 
     def exc(self):
 
@@ -41,7 +48,6 @@ class Decomposing:
         for code in codes:
             logging.info(code)
 
-            data = self.__get_data(code=code)
-            parts = self.__decompose(blob=data)
-
-
+            frame = self.__get_data(code=code)
+            parts = self.__decompose(frame=frame)
+            data = self.__exc_structuring(parts=parts)
