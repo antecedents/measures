a20e86244a2527aa6dad46f152d15218ad86d18c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-15 18:16:02 +0000

    Update src/algorithms/graphing.py

--- src/algorithms/graphing.py
+++ src/algorithms/graphing.py
@@ -1,5 +1,25 @@
+import pandas as pd
+import statsmodels.tsa.seasonal as stl
+
 
 class Graphing:
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def __frame(struct: stl.DecomposeResult):
+
+        decompositions = pd.DataFrame(
+            data={'observation': struct.observed.values, 'trend': struct.trend.values, 'seasonal': struct.seasonal.values,
+                  'residue': struct.resid.values, 'weight': struct.weights.values}, index=struct.observed.index)
+        decompositions.reset_index(inplace=True)
+        decompositions.sort_values(by='week_ending_date', inplace=True)
+
+        return decompositions
+
+    def exc(self, struct: stl.DecomposeResult):
+
+        self.__frame(struct=struct)
+
+
