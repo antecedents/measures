a5be53840e10acc349d6fcc20eb64af59f12bc46
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-15 20:14:17 +0000

    Update src/algorithms/drawing.py

--- src/algorithms/drawing.py
+++ src/algorithms/drawing.py
@@ -1,41 +1,8 @@
 import pandas as pd
 import numpy as np
-import statsmodels.tsa.seasonal as stl
 
 
 class Drawing:
 
     def __init__(self):
         pass
-
-    @staticmethod
-    def __epoch(blob: pd.DataFrame):
-        """
-
-        :param blob:
-        :return:
-        """
-
-        decompositions = blob.copy()
-
-        decompositions['epoch']  = (
-                decompositions['week_ending_date'].to_numpy().astype(np.int64) / (10 ** 6)
-        ).astype(np.longlong)
-
-    @staticmethod
-    def __frame(struct: stl.DecomposeResult):
-
-        decompositions = pd.DataFrame(
-            data={'observation': struct.observed.values, 'trend': struct.trend.values, 'seasonal': struct.seasonal.values,
-                  'residue': struct.resid.values, 'weight': struct.weights.values}, index=struct.observed.index)
-        decompositions.reset_index(inplace=True)
-        decompositions.sort_values(by='week_ending_date', inplace=True)
-
-        return decompositions
-
-    def exc(self, struct: stl.DecomposeResult):
-
-        data = self.__frame(struct=struct)
-        data = self.__epoch(blob=data)
-
-
