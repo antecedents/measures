7f6d6c0d21136e40d5c4288fca4444cb61e5f7c7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-15 14:24:11 +0000

    Add config.py

---
+++ config.py
@@ -0,0 +1,41 @@
+"""
+Module config.py
+"""
+import os
+
+
+class Config:
+    """
+    Description
+    -----------
+
+    A class for configurations
+    """
+
+    def __init__(self) -> None:
+        """
+        Notes<br>
+        -------<br>
+
+        <a href="https://otexts.com/fpp2/stationarity.html">Stationarity</a>
+        """
+
+        self.warehouse = os.path.join(os.getcwd(), 'warehouse')
+        self.decompositions_ = os.path.join(self.warehouse, 'decompositions')
+
+        # Seed
+        self.seed = 5
+
+        # Configuration files
+        self.s3_parameters_key = 's3_parameters.yaml'
+
+        '''
+        For architecture JSON
+        '''
+
+        # Fields
+        self.fields = ['week_ending_date', 'health_board_code', 'hospital_code',  'n_attendances']
+
+        # Seasons, trends, etc.
+        self.seasons = 52
+        self.trends = 1
