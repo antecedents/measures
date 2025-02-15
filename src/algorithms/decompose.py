f53439ea16f4dfe4f3b803cced2d08e93bf667ee
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-15 17:27:50 +0000

    Update src/algorithms/decompose.py

--- src/algorithms/decompose.py
+++ src/algorithms/decompose.py
@@ -1,3 +1,16 @@
 
+import pandas as pd
+import numpy as np
+import dask
+
+import statsmodels.tsa.seasonal as stsl
+
 class Decompose:
-    pass
+
+    def __init__(self, data: pd.DataFrame):
+
+        self.__data = data
+
+    def exc(self):
+
+        codes: np.ndarray = self.__data['hospital_code'].unique()
