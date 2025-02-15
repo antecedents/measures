2bfe69235527cfcd20e0f288fbc9bb3f72d00cdd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-15 15:25:31 +0000

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -32,7 +32,7 @@ class Interface:
 
         # Logging
         logging.basicConfig(level=logging.INFO,
-                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n\n',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
