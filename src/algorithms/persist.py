5e31e6657c7442c084a6ca599f83fc51f58cb28d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-02-15 21:34:01 +0000

    Update src/algorithms/persist.py

--- src/algorithms/persist.py
+++ src/algorithms/persist.py
@@ -1,20 +1,34 @@
 import pandas as pd
-import numpy as np
 
 
-class Drawing:
+class Persist:
 
     def __init__(self):
+        """
+        Constructor
+        """
 
         self.__fields = ['milliseconds', 'observation', 'trend', 'seasonal', 'residue', 'weight']
 
     def __get_instances(self, blob: pd.DataFrame) -> dict:
+        """
+
+        :param blob:
+        :return:
+        """
 
         instances = blob[self.__fields].to_dict(orient='split')
 
         return instances
 
     def exc(self, data: pd.DataFrame, health_board_code: str, hospital_code):
+        """
+
+        :param data:
+        :param health_board_code:
+        :param hospital_code:
+        :return:
+        """
 
         instances: dict = self.__get_instances(blob=data)
         instances['health_board_code'] = health_board_code
