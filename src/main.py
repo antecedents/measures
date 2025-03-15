9bcc06a0eeafac8485ca9d179995a80d6765363c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-15 17:51:43 +0000

    Update src/main.py: In Progress

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ import logging
 import os
 import sys
 
-import boto3
-
 
 def main():
     """
@@ -13,16 +11,6 @@ def main():
     """
 
     logger: logging.Logger = logging.getLogger(__name__)
-
-
-
-    '''
-    Set up
-    '''
-    # setup: bool = src.preface.setup.Setup(service=service, s3_parameters=s3_parameters).exc()
-    # if not setup:
-    #     src.functions.cache.Cache().exc()
-    #     sys.exit('No Executions')
 
     '''
     Steps
@@ -50,18 +38,9 @@ if __name__ == '__main__':
     import src.data.interface
     import src.decomposition.decomposing
     import src.functions.cache
-    import src.functions.service
-    import src.s3.s3_parameters
-    import src.preface.setup
+    import src.preface.interface
     import src.transfer.interface
 
-    import src.preface.interface
-
     connector, s3_parameters, service = src.preface.interface.Interface().exc()
-
-    # S3 S3Parameters, Service Instance
-    # connector = boto3.session.Session()
-    # s3_parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
-    # service = src.functions.service.Service(connector=connector, region_name=s3_parameters.region_name).exc()
 
     main()
