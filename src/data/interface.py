27c8d449bfb08a3851167dd621a95df71029b47b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-15 17:40:09 +0000

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -1,10 +1,10 @@
 """Module interface.py"""
-import pandas as pd
+import logging
 
 import config
 import src.elements.s3_parameters as s3p
-import src.elements.text_attributes as txa
-import src.functions.streams
+import src.s3.directives
+import src.s3.unload
 
 
 class Interface:
@@ -12,76 +12,51 @@ class Interface:
     Notes<br>
     ------<br>
 
-    Reads-in the data in focus.
+    An interface to the data/artefacts retrieval class.  <b>Beware, sometimes dask
+    will be unnecessary, edit accordingly.</b>
     """
 
     def __init__(self, s3_parameters: s3p.S3Parameters):
         """
 
-        :param s3_parameters: The overarching S3 parameters settings of this project, e.g., region code
-                              name, buckets, etc.
+        :param s3_parameters: The overarching S3 (Simple Storage Service) parameters
+                              settings of this project, e.g., region code name, buckets, etc.
         """
 
-        self.__s3_parameters = s3_parameters
+        self.__s3_parameters: s3p.S3Parameters = s3_parameters
 
-        # Instances
-        self.__streams = src.functions.streams.Streams()
+        # Setting up
         self.__configurations = config.Config()
+        self.__source_bucket = self.__s3_parameters.internal
+        self.__prefix = self.__s3_parameters.path_internal_artefacts + self.__configurations.stamp
 
-    def __get_data(self, uri: str) -> pd.DataFrame:
+        # Directives
+        self.__directives = src.s3.directives.Directives()
+
+    def __get_assets(self, source_bucket: str, origin: str, target: str):
         """
 
-        :param uri: A data sets' uniform resource identifier.
+        :param source_bucket:
+        :param origin:
+        :param target:
         :return:
         """
 
-        text = txa.TextAttributes(uri=uri, header=0)
+        return self.__directives.synchronise(
+            source_bucket=source_bucket, origin=origin, target=target)
 
-        frame = self.__streams.read(text=text)
-        frame['week_ending_date'] = pd.to_datetime(
-            frame['week_ending_date'].astype(dtype=str), errors='coerce', format='%Y-%m-%d')
-
-        return frame[self.__configurations.fields]
-
-    def __viable(self, blob: pd.DataFrame) -> pd.DataFrame:
+    def exc(self):
         """
 
-        :param blob:
         :return:
         """
 
-        # Counts per institution
-        counts: pd.DataFrame = blob.copy()[['health_board_code', 'hospital_code']].groupby(
-            by='health_board_code').value_counts().to_frame()
-        counts.reset_index(inplace=True)
+        try:
+            state = self.__get_assets(
+                source_bucket=self.__source_bucket,
+                origin=self.__prefix,
+                target=self.__configurations.data_)
+        except RuntimeError as err:
+            raise err from err
 
-        # Institutions that have a viable number of observations.
-        viable: pd.DataFrame = counts.loc[counts['count'] >= (
-                self.__configurations.seasons * self.__configurations.cycles), :]
-
-        return viable
-
-    def exc(self, stamp: str) -> pd.DataFrame:
-        """
-
-        :param stamp: A date stamp.
-        :return:
-            training: The training data set.<br>
-            testing: The testing data set.
-        """
-
-        # The data sets' uniform resource identifier
-        uri = ('s3://' + self.__s3_parameters.internal + '/' +
-                self.__s3_parameters.path_internal_data + f'raw/{stamp}.csv')
-
-        # Reading
-        data = self.__get_data(uri=uri)
-
-        # Institutions that have a viable number of observations.
-        viable = self.__viable(blob=data)
-        data = data.copy().loc[data['hospital_code'].isin(viable['hospital_code'].unique()), :]
-
-        # Index
-        data.set_index(keys='week_ending_date', drop=True, inplace=True)
-
-        return data
+        logging.info(state)
