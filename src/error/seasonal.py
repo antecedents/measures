e9073a4a3d43180e41ff824d18618d2cfc6a6a3a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-17 12:26:48 +0000

    Add src/error/seasonal.py

---
+++ src/error/seasonal.py
@@ -0,0 +1,54 @@
+import os
+
+import pandas as pd
+
+import config
+import src.elements.seasonal as sa
+import src.functions.objects
+
+
+class Seasonal:
+    """
+
+    """
+
+    def __init__(self, code: str):
+        """
+
+        :param code: The identification code of an institution/hospital
+        """
+
+        self.__configurations = config.Config()
+
+        # For reading JSON files
+        self.__objects = src.functions.objects.Objects()
+
+        # Reading-in the seasonal component forecasting data
+        uri = os.path.join(self.__configurations.data_, 'models', code, 'scf_estimates.json')
+        self.__data = self.__objects.read(uri=uri)
+
+    def __get_section(self, section: str):
+
+        frame = pd.DataFrame.from_dict(self.__data[section], orient='tight')
+
+        frame['week_ending_date'] = pd.to_datetime(
+            frame['date'].astype(str), errors='coerce', format='%Y-%m-%d')
+
+        frame.drop(columns='date', inplace=True)
+        frame.sort_values(by='week_ending_date', ascending=True, inplace=True)
+
+        return frame
+
+    def exc(self):
+
+        sa.Seasonal(
+            estimates=self.__get_section(section='estimates'),
+            tests=self.__get_section(section='tests'),
+            futures=self.__get_section(section='futures'))
+
+
+
+
+
+
+
