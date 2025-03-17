221f99819fb074f1f5880a368288ab0c6b38bea9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-17 21:48:11 +0000

    Update src/error/trend.py

---
+++ src/error/trend.py
@@ -0,0 +1,51 @@
+"""Module trend.py"""
+import os
+
+import pandas as pd
+
+import config
+import src.elements.text_attributes as txa
+import src.functions.streams
+
+
+class Trend:
+    """
+    <b>Notes</b><br>
+    ------<br>
+
+    Reads-in the trend component forecasting estimations<br>
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        self.__configurations = config.Config()
+
+        self.__streams = src.functions.streams.Streams()
+
+
+
+    def __get_data(self, uri: str) -> pd.DataFrame:
+        """
+
+        :param uri:
+        :return:
+        """
+
+        text = txa.TextAttributes(uri=uri, header=0)
+
+        frame = self.__streams.read(text=text)
+        frame['week_ending_date'] = pd.to_datetime(
+            frame['date'].astype(str), errors='coerce', format='%Y-%m-%d')
+        frame.drop(columns='date', inplace=True)
+        frame.sort_values(by='week_ending_date', ascending=True, inplace=True)
+
+        return frame
+
+    def exc(self, code: str):
+
+        uri = os.path.join(self.__configurations.data_, 'models', code, 'tcf_forecasts.json')
+
+        return self.__get_data(uri=uri)
