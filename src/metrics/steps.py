867bed1d267b1adbfbe19b534f03c847c069a940
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-17 01:57:02 +0000

    Update src/metrics/steps.py: In Progress

--- src/metrics/steps.py
+++ src/metrics/steps.py
@@ -9,22 +9,29 @@ import src.functions.objects
 
 
 class Steps:
+    """
+    Steps
+    """
 
     def __init__(self):
+        """
+
+        """
 
         self.__configurations = config.Config()
         self.__objects = src.functions.objects.Objects()
 
     def exc(self):
+        """
 
-        logging.info('Starting ....')
+        :return:
+        """
 
         listings = glob.glob(
             pathname=os.path.join(self.__configurations.data_, 'models', '**', 'scf_estimates.json'))
         logging.info(listings)
 
         for listing in listings:
-
             nodes = self.__objects.read(uri=listing)
-            estimates = pd.DataFrame.from_dict(nodes['estimates'], orient='index')
+            estimates = pd.DataFrame.from_dict(nodes['estimates'], orient='tight')
             logging.info(estimates.head())
