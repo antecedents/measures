32b9ee7e578557b0e2bf8f1db7015c571d615e14
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-17 01:55:28 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
 """Module main.py"""
+import datetime
+import logging
 import os
 import sys
 
 
 def main():
     """
+    https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
 
     :return:
     """
 
+    logger = logging.getLogger(__name__)
+    logger.info(datetime.datetime.now().strftime('%A %d %b %Y, %H:%M:%S.%f'))
+
     # Steps
     # src.data.interface.Interface(s3_parameters=s3_parameters).exc()
-    # src.decompositions.interface.Interface().exc()
-    src.metrics.interface.Interface().exc()
+    src.decompositions.interface.Interface().exc()
+    src.metrics.steps.Steps().exc()
 
     # Delete cache
     src.functions.cache.Cache().exc()
@@ -24,11 +30,17 @@ if __name__ == '__main__':
     sys.path.append(root)
     sys.path.append(os.path.join(root, 'src'))
 
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n\n',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+
     # Classes
     import src.data.interface
     import src.decompositions.interface
     import src.functions.cache
-    import src.metrics.interface
+    import src.metrics.steps
     import src.preface.interface
     import src.transfer.interface
 
