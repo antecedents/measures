ce98e3f6ca8f993956d52b48ab6738cbacd02242
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-17 16:36:22 +0000

    Update src/error/seasonal.py

--- src/error/seasonal.py
+++ src/error/seasonal.py
@@ -1,5 +1,6 @@
-import os
+"""Module seasonal.py"""
 import logging
+import os
 
 import pandas as pd
 
@@ -10,13 +11,16 @@ import src.functions.objects
 
 class Seasonal:
     """
+    <b>Notes</b><br>
+    ------<br>
 
+    Retrieves the seasonal component forecasting estimations<br>
     """
 
     def __init__(self, code: str):
         """
 
-        :param code: The identification code of an institution/hospital
+        :param code: The identification code of an institution/hospital.
         """
 
         self.__configurations = config.Config()
@@ -26,22 +30,30 @@ class Seasonal:
 
         # Reading-in the seasonal component forecasting data
         uri = os.path.join(self.__configurations.data_, 'models', code, 'scf_estimates.json')
-        logging.info(uri)
+
         self.__data = self.__objects.read(uri=uri)
 
     def __get_section(self, section: str):
+        """
+
+        :param section: A data section of the estimations dictionary
+        :return:
+        """
 
         frame = pd.DataFrame.from_dict(self.__data[section], orient='tight')
 
         frame['week_ending_date'] = pd.to_datetime(
             frame['date'].astype(str), errors='coerce', format='%Y-%m-%d')
-
         frame.drop(columns='date', inplace=True)
         frame.sort_values(by='week_ending_date', ascending=True, inplace=True)
 
         return frame
 
     def exc(self):
+        """
+
+        :return:
+        """
 
         return sa.Seasonal(
             estimates=self.__get_section(section='estimates'),
