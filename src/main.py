2fed7e44b32444df778e403d09a754f778bbbe4c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-17 01:27:22 +0000

    Update src/main.py: In Progress

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 """Module main.py"""
-import logging
 import os
 import sys
 
@@ -10,11 +9,10 @@ def main():
     :return:
     """
 
-    logger: logging.Logger = logging.getLogger(__name__)
-
     # Steps
     # src.data.interface.Interface(s3_parameters=s3_parameters).exc()
-    src.decompositions.interface.Interface().exc()
+    # src.decompositions.interface.Interface().exc()
+    src.metrics.interface.Interface().exc()
 
     # Delete cache
     src.functions.cache.Cache().exc()
@@ -26,17 +24,13 @@ if __name__ == '__main__':
     sys.path.append(root)
     sys.path.append(os.path.join(root, 'src'))
 
-    # Logging
-    logging.basicConfig(level=logging.INFO,
-                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n\n',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-
     # Classes
     import src.data.interface
+    import src.decompositions.interface
     import src.functions.cache
+    import src.metrics.interface
     import src.preface.interface
     import src.transfer.interface
-    import src.decompositions.interface
 
     # connector, s3_parameters, service = src.preface.interface.Interface().exc()
 
