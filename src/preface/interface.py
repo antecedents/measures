c0f4b53f88954f1a9955c44e945cca816037c0a2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-18 21:52:05 +0000

    Update src/preface/interface.py: Testing Mode

--- src/preface/interface.py
+++ src/preface/interface.py
@@ -7,6 +7,7 @@ import src.elements.s3_parameters as s3p
 import src.elements.service as sr
 import src.functions.service
 import src.preface.setup
+import src.s3.configurations
 import src.s3.s3_parameters
 
 
@@ -18,6 +19,16 @@ class Interface:
     def __init__(self):
         pass
 
+    @staticmethod
+    def __get_arguments(connector: boto3.session.Session) -> dict:
+        """
+
+        :return:
+        """
+
+        key_name = 'artefacts' + '/' + 'architecture' + '/' + 'single' + '/' + 'futures' + '/' + 'arguments.json'
+
+        return src.s3.configurations.Configurations(connector=connector).objects(key_name=key_name)
 
     @staticmethod
     def __setting_up(service: sr.Service, s3_parameters: s3p.S3Parameters):
@@ -30,7 +41,7 @@ class Interface:
 
         src.preface.setup.Setup(service=service, s3_parameters=s3_parameters).exc()
 
-    def exc(self) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service]:
+    def exc(self) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
         """
 
         :return:
@@ -40,8 +51,9 @@ class Interface:
         s3_parameters: s3p.S3Parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
         service: sr.Service = src.functions.service.Service(
             connector=connector, region_name=s3_parameters.region_name).exc()
+        arguments: dict = self.__get_arguments(connector=connector)
 
 
-        self.__setting_up(service=service, s3_parameters=s3_parameters)
+        # self.__setting_up(service=service, s3_parameters=s3_parameters)
 
-        return connector, s3_parameters, service
+        return connector, s3_parameters, service, arguments
