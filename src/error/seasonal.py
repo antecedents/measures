dfb62420c20f11e16f0c29f71e5e1f8ed741b9de
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-18 12:00:51 +0000

    Update src/error/seasonal.py

--- src/error/seasonal.py
+++ src/error/seasonal.py
@@ -16,28 +16,23 @@ class Seasonal:
     Retrieves the seasonal component forecasting estimations<br>
     """
 
-    def __init__(self, code: str):
+    def __init__(self):
         """
-
-        :param code: The identification code of an institution/hospital.
+        Constructor
         """
 
         self.__configurations = config.Config()
 
-        # Data path
-        uri = os.path.join(self.__configurations.data_, 'models', code, 'scf_estimates.json')
-
-        # Reading-in the seasonal component forecasting data
-        self.__data = src.functions.objects.Objects().read(uri=uri)
-
-    def __get_section(self, section: str) ->pd.DataFrame:
+    @staticmethod
+    def __get_section(data: dict, section: str) ->pd.DataFrame:
         """
 
+        :param data:
         :param section: A data section of the estimations dictionary
         :return:
         """
 
-        frame = pd.DataFrame.from_dict(self.__data[section], orient='tight')
+        frame = pd.DataFrame.from_dict(data[section], orient='tight')
 
         frame['week_ending_date'] = pd.to_datetime(
             frame['date'].astype(str), errors='coerce', format='%Y-%m-%d')
@@ -46,13 +41,18 @@ class Seasonal:
 
         return frame
 
-    def exc(self) -> sa.Seasonal:
+    def exc(self, code: str) -> sa.Seasonal:
         """
 
+        :param code: The identification code of an institution/hospital.
         :return:
         """
 
+        # Reading-in the seasonal component forecasting data
+        uri = os.path.join(self.__configurations.data_, 'models', code, 'scf_estimates.json')
+        data = src.functions.objects.Objects().read(uri=uri)
+
         return sa.Seasonal(
-            estimates=self.__get_section(section='estimates').drop(columns='seasonal'),
-            tests=self.__get_section(section='tests'),
-            futures=self.__get_section(section='futures'))
+            estimates=self.__get_section(data=data, section='estimates').drop(columns='seasonal'),
+            tests=self.__get_section(data=data, section='tests'),
+            futures=self.__get_section(data=data, section='futures'))
