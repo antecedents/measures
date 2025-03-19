57c27bb870bf2b237dccb57bf0b0f4c6989632d6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-19 16:59:33 +0000

    Update src/error/points.py: In Progress

--- src/error/points.py
+++ src/error/points.py
@@ -1,4 +1,4 @@
-import logging
+
 import pandas as pd
 import numpy as np
 
@@ -10,7 +10,10 @@ class Points:
 
     def __init__(self):
 
-        self.__span = 0.5
+        self.__span = 0.90
+
+        # self__rename = {'seasonal_est': 'sc_estimate', 'mu': 'tc_estimate',
+        #                'std': 'tc_estimate_deviation'}
 
     @staticmethod
     def __milliseconds(blob: pd.DataFrame):
@@ -20,7 +23,6 @@ class Points:
                 frame['week_ending_date'].to_numpy().astype(np.int64) / (10 ** 6)
         ).astype(np.longlong)
         frame.sort_values(by='week_ending_date', inplace=True)
-
 
     @staticmethod
     def __metric(period: float, average: float, deviation: float, percentile: float) -> float:
@@ -38,7 +40,6 @@ class Points:
 
         return period + average + (score * deviation)
 
-
     def exc(self, seasonal: sa.Seasonal, trend: pd.DataFrame):
         """
         Focus
@@ -52,13 +53,18 @@ class Points:
         training = seasonal.estimates.merge(trend, how='left', on='week_ending_date')
         testing = seasonal.tests.merge(trend, how='left', on='week_ending_date')
         futures = seasonal.futures.merge(trend, how='left', on='week_ending_date')
-        futures['ln'] = np.nan
+        futures['n_attendances'] = np.nan
 
-        fields = ['week_ending_date', 'ln', 'seasonal_est', 'mu', 'std']
+        fields = ['week_ending_date', 'n_attendances', 'seasonal_est', 'mu', 'std']
         data = pd.concat((training[fields], testing[fields], futures[fields]), axis=0, ignore_index=True)
 
-        data['upper'] = self.__metric(
+        data['u_estimate'] = self.__metric(
             period = data['seasonal_est'], average=data['mu'], deviation=data['std'], percentile=(0.5 + 0.5*self.__span))
-        data['lower'] = self.__metric(
+        data['l_estimate'] = self.__metric(
             period = data['seasonal_est'], average=data['mu'], deviation=data['std'], percentile=(0.5 - 0.5*self.__span))
-        logging.info(data)
+
+        # sum(|error|)/N <- mae
+        # sum(error.^2)/N <- mse
+        # sqrt(mse) <- rmse
+        # 100*sum(abs(error./original))/N <- mape
+
