577da73d594a7af9e31ab714871198958edea89c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-20 14:16:02 +0000

    Update src/forecasts/metrics.py

--- src/forecasts/metrics.py
+++ src/forecasts/metrics.py
@@ -20,7 +20,7 @@ class Metrics:
         :return:
         """
 
-        se: np.ndarray = np.power(data[['l_error', 'u_error']].to_numpy(), 2)
+        se: np.ndarray = np.power(data[['l_e_error', 'u_e_error']].to_numpy(), 2)
         mse = np.sum(se, axis=0)/se.shape[0]
         logging.info(np.sqrt(mse))
 
@@ -32,7 +32,7 @@ class Metrics:
         :return:
         """
 
-        er = np.absolute(data[['l_error_rate', 'u_error_rate']].to_numpy())
+        er = np.absolute(data[['l_e_error_rate', 'u_e_error_rate']].to_numpy())
         mer = np.sum(er, axis=0)/er.shape[0]
         logging.info(100*mer)
 
@@ -44,7 +44,7 @@ class Metrics:
         :return:
         """
 
-        er = np.absolute(data[['l_error_rate', 'u_error_rate']].to_numpy())
+        er = np.absolute(data[['l_e_error_rate', 'u_e_error_rate']].to_numpy())
         logging.info(100*np.median(er, axis=0))
 
     def exc(self, parts: pr.Parts):
@@ -59,5 +59,5 @@ class Metrics:
         self.__average_percentage(data=data)
         self.__median_percentage(data=data)
 
-        er = np.absolute(data[['l_error_rate', 'u_error_rate']].to_numpy())
+        er = np.absolute(data[['l_e_error_rate', 'u_e_error_rate']].to_numpy())
         logging.info(np.percentile(a=er, q=[10, 25, 50, 75, 90], axis=0))
