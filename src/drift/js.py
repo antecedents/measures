742cdd744f4aede36bde066fb97fd8d2271c87bd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-20 23:56:51 +0000

    Update src/drift/js.py

--- src/drift/js.py
+++ src/drift/js.py
@@ -2,4 +2,16 @@
 class JS:
 
     def __init__(self):
-        pass
+        """
+
+        """
+
+    def exc(self):
+        """
+        n_of_cycle=52
+        points=excerpt["n_attendances"].to_numpy()
+        reverse = points[::-1]
+        li.hankel(reverse[:n_of_cycle], reverse[(n_of_cycle - 1):]).T
+
+        :return:
+        """
