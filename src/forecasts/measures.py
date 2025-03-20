38af91683cc2b29dd8f6ed3d67cbc0b392ecc7d2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-20 14:15:52 +0000

    Update src/forecasts/measures.py

--- src/forecasts/measures.py
+++ src/forecasts/measures.py
@@ -1,4 +1,4 @@
-import logging
+"""Module measures.py"""
 import numpy as np
 import pandas as pd
 
@@ -13,26 +13,34 @@ class Measures:
     @staticmethod
     def __errors(data: pd.DataFrame) -> pd.DataFrame:
         """
+        errors.sort(axis=1)
 
-        :param data:
+        :param data: The forecasts w.r.t. training or testing phases.
         :return:
         """
 
+        # ground truth
         ground = data['n_attendances'].to_numpy()[:,None]
-        forecasts = data[['l_estimate', 'u_estimate']].to_numpy()
-        errors: np.ndarray =  ground - forecasts
-        errors.sort(axis=1)
 
-        data.loc[:, ['l_error', 'u_error']] = errors
-        data.loc[:, ['l_error_rate', 'u_error_rate']] = np.true_divide(errors, ground)
+        # forecasts
+        forecasts = data[['l_estimate', 'u_estimate']].to_numpy()
+
+        # raw errors and error rates
+        errors: np.ndarray =  ground - forecasts
+        data.loc[:, ['l_e_error', 'u_e_error']] = errors
+        data.loc[:, ['l_e_error_rate', 'u_e_error_rate']] = np.true_divide(errors, ground)
 
         return data
 
     def exc(self, parts: pr.Parts) -> pr.Parts:
+        """
+
+        :param parts: An institution's data object consisting of forecasts w.r.t. training,
+                      testing, and futures phases.
+        :return:
+        """
 
         parts = parts._replace(estimates=self.__errors(data=parts.estimates.copy()),
                        tests=self.__errors(data=parts.tests.copy()))
-
-        logging.info(parts)
 
         return parts
