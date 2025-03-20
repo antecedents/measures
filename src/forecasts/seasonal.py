8edbe74021f67fc1492910e74302554da73e02c3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-20 18:09:16 +0000

    Update src/forecasts/seasonal.py

--- src/forecasts/seasonal.py
+++ src/forecasts/seasonal.py
@@ -1,6 +1,7 @@
 """Module seasonal.py"""
 import os
 
+import numpy as np
 import pandas as pd
 
 import config
@@ -33,10 +34,18 @@ class Seasonal:
         """
 
         frame = pd.DataFrame.from_dict(data[section], orient='tight')
+        frame.rename(columns={'date': 'week_ending_date'}, inplace=True)
 
+        # ascertain datetime dates
         frame['week_ending_date'] = pd.to_datetime(
-            frame['date'].astype(str), errors='coerce', format='%Y-%m-%d')
-        frame.drop(columns='date', inplace=True)
+            frame['week_ending_date'].astype(str), errors='coerce', format='%Y-%m-%d')
+
+        # the milliseconds epoch for graphing
+        frame['milliseconds']  = (
+                frame['week_ending_date'].to_numpy().astype(np.int64) / (10 ** 6)
+        ).astype(np.longlong)
+
+        # sort
         frame.sort_values(by='week_ending_date', ascending=True, inplace=True)
 
         return frame
