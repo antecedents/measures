56ce28ef47ed929b82f9d0da800d04c48ac1bbb3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-20 01:27:44 +0000

    Update src/main.py: In Progress

--- src/main.py
+++ src/main.py
@@ -17,9 +17,9 @@ def main():
 
     # Steps
     # src.data.interface.Interface(s3_parameters=s3_parameters).exc()
-    src.decompositions.interface.Interface().exc()
-    src.error.interface.Interface().exc()
-    src.drift.interface.Interface(arguments=arguments).exc()
+    # src.decompositions.interface.Interface().exc()
+    # src.error.interface.Interface(arguments=arguments).exc()
+    # src.drift.interface.Interface(arguments=arguments).exc()
 
     # Delete cache
     src.functions.cache.Cache().exc()
