60f295a52265f8e4e4255279dc0ba4066439d931
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-21 20:56:27 +0000

    Update src/drift/metrics.py

--- src/drift/metrics.py
+++ src/drift/metrics.py
@@ -1,4 +1,5 @@
 import logging
+import typing
 
 import numpy as np
 import pandas as pd
@@ -13,14 +14,40 @@ class Metrics:
 
     @staticmethod
     def __get_js(penultimate: np.ndarray, ultimate: np.ndarray):
+        """
+
+        :param penultimate:
+        :param ultimate:
+        :return:
+        """
 
         # noinspection PyArgumentList
         return spa.distance.jensenshannon(p=penultimate, q=ultimate, axis=1)
 
     @staticmethod
     def __get_wasserstein(penultimate: np.ndarray, ultimate: np.ndarray) -> float:
+        """
 
+        :param penultimate:
+        :param ultimate:
+        :return:
+        """
+
+        # noinspection PyTypeChecker
         return sta.wasserstein_distance(penultimate, ultimate).__float__()
+
+    @staticmethod
+    def __get_matrices(matrix: np.ndarray) -> typing.Tuple[np.ndarray, np.ndarray]:
+        """
+
+        :param matrix:
+        :return:
+        """
+
+        penultimate = matrix[1:, :]
+        ultimate = matrix[:-1, :]
+
+        return np.fliplr(penultimate), np.fliplr(ultimate)
 
     def exc(self, matrix: np.ndarray) -> tuple:
         """
@@ -29,17 +56,10 @@ class Metrics:
         :return:
         """
 
-        ultimate = matrix[:-1, :]
-        penultimate = matrix[1:, :]
-
-        ultimate = np.fliplr(ultimate)
-        penultimate = np.fliplr(penultimate)
+        penultimate, ultimate = self.__get_matrices(matrix=matrix)
 
         js = self.__get_js(penultimate=penultimate, ultimate=ultimate)
-        logging.info(len(js))
-
         wasserstein = [self.__get_wasserstein(penultimate[i,:], ultimate[i,:]) for i in np.arange(ultimate.shape[0])]
-        logging.info(type(wasserstein))
-
+        logging.info('SCORES: %s, %s', type(js), type(wasserstein))
 
         return matrix.shape
