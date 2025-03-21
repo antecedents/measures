9f6c5c3aa888d4664effde591359a5cb8bc4bb2d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-21 16:06:03 +0000

    Add src/drift/hankel.py

---
+++ src/drift/hankel.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+import pandas as pd
+import scipy.linalg as li
+
+class Hankel:
+
+    def __init__(self, arguments: dict):
+
+        self.__arguments = arguments
+
+    def exc(self, data: pd.DataFrame) -> np.ndarray:
+        """
+
+        :param data: An institution's data
+        :return:
+        """
+
+        frame = data.copy()
+
+        points = frame['n_attendances'].to_numpy()
+        reverse = points[::-1]
+
+        matrix: np.ndarray = li.hankel(
+            reverse[:self.__arguments.get('seasons')],
+            reverse[(self.__arguments.get('seasons') - 1):]).T
+
+        return matrix
+
