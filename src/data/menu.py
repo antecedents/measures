fc02260d8afe06d9e41f281ea4731d707f16116d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-22 18:36:54 +0000

    Add src/data/menu.py

---
+++ src/data/menu.py
@@ -0,0 +1,34 @@
+"""Module menu.py"""
+import glob
+import logging
+import os
+
+import pandas as pd
+
+import config
+import src.functions.objects
+
+
+class Menu:
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        self.__configurations = config.Config()
+
+    def exc(self):
+        """
+
+        :return:
+        """
+
+        listings = glob.glob(os.path.join(self.__configurations.data_, 'data', '**'))
+        codes = [os.path.basename(listing) for listing in listings]
+
+        frame = pd.DataFrame(data={'desc': codes, 'name': codes})
+        nodes = frame.to_dict(orient='records')
+
+        src.functions.objects.Objects().write(
+            nodes=nodes, path=os.path.join(self.__configurations.menu_, 'menu.json'))
