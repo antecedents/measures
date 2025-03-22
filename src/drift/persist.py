437f0333d9fb87535217db1082cb5da5f6ba510b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-22 15:55:09 +0000

    Update src/drift/persist.py

--- src/drift/persist.py
+++ src/drift/persist.py
@@ -1,20 +1,60 @@
 """Module persist.py"""
+import logging
+import os
 import numpy as np
 import pandas as pd
+import json
+
+import config
+import src.functions.objects
+import src.functions.directories
 
 
 class Persist:
 
     def __init__(self):
-        pass
+        """
 
-    @staticmethod
-    def __milliseconds(blob: pd.DataFrame) -> pd.DataFrame:
+        """
 
-        frame = blob.copy()
-        frame['milliseconds']  = (
-                frame['week_ending_date'].to_numpy().astype(np.int64) / (10 ** 6)
-        ).astype(np.longlong)
-        frame.sort_values(by='week_ending_date', inplace=True)
+        self.__fields = ['milliseconds', 'js', 'wasserstein', 'date']
 
-        return frame
+        self.__configurations = config.Config()
+        self.__objects = src.functions.objects.Objects()
+
+        # Storage
+        self.__path = os.path.join(self.__configurations.warehouse, 'drift')
+        src.functions.directories.Directories().create(self.__path)
+
+    def __get_dictionary(self, frame: pd.DataFrame):
+        """
+
+        :param frame:
+        :param code:
+        :return:
+        """
+
+        string: str = frame[self.__fields].to_json(orient='split')
+        dictionary: dict = json.loads(string)
+
+        code: pd.Series = frame[['health_board_code', 'hospital_code']].drop_duplicates().squeeze()
+        dictionary['health_board_code'] = code.health_board_code
+        dictionary['hospital_code'] = code.hospital_code
+
+        return dictionary
+
+    def exc(self, frame: pd.DataFrame, code: str) -> str:
+        """
+
+        :param frame:
+        :param code:
+        :return:
+        """
+
+        # Dictionary
+        dictionary = self.__get_dictionary(frame=frame)
+
+        message = self.__objects.write(
+            nodes=dictionary, path=os.path.join(self.__path, f'{code}'))
+
+        return message
