45db7d14a407bf425f6a1d1409aa178da1f9e723
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-22 21:10:51 +0000

    Update src/data/reference.py

--- src/data/reference.py
+++ src/data/reference.py
@@ -1,14 +1,37 @@
+import logging
+import src.elements.s3_parameters as s3p
+import src.functions.streams
+import src.elements.text_attributes as txa
+
 
 class References:
 
-    def __init__(self):
-        pass
+    def __init__(self, s3_parameters: s3p.S3Parameters):
+        """
+
+        :param s3_parameters: The overarching S3 (Simple Storage Service) parameters
+                              settings of this project, e.g., region code name, buckets, etc.
+        """
+
+        self.__s3_parameters: s3p.S3Parameters = s3_parameters
+
+        self.__stream = src.functions.streams.Streams()
 
     def __boards(self):
-        pass
+
+        uri = 's3://' + self.__s3_parameters.internal + '/' + 'references' + '/' + 'boards.csv'
+        text = txa.TextAttributes(uri=uri, header=0, usecols=['health_board_code', 'health_board_name'])
+
+        return self.__stream.read(text=text)
 
     def __institutions(self):
-        pass
+
+        uri = 's3://' + self.__s3_parameters.internal + '/' + 'references' + '/' + 'institutions.csv'
+        text = txa.TextAttributes(uri=uri, header=0, usecols=['health_board_code', 'hospital_code', 'hospital_name'])
+
+        return self.__stream.read(text=text)
 
     def exc(self):
-        pass
+
+        logging.info(self.__boards())
+        logging.info(self.__institutions())
