35672c9226392a38a84580e6478a6dd7ca80d603
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-22 20:51:14 +0000

    Add src/data/reference.py

---
+++ src/data/reference.py
@@ -0,0 +1,14 @@
+
+class References:
+
+    def __init__(self):
+        pass
+
+    def __boards(self):
+        pass
+
+    def __institutions(self):
+        pass
+
+    def exc(self):
+        pass
