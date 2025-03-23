fe0f7fafd4102c359e8c2f1ba0da58264e105642
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-23 02:32:44 +0000

    Update src/decompositions/interface.py

--- src/decompositions/interface.py
+++ src/decompositions/interface.py
@@ -18,10 +18,13 @@ class Interface:
     The interface to the programs that prepare the appropriate data structures for graphing the decompositions.
     """
 
-    def __init__(self):
+    def __init__(self, reference: pd.DataFrame):
         """
-        Constructor
+
+        :param reference:
         """
+
+        self.__reference = reference
 
         self.__configurations = config.Config()
         self.__streams = src.functions.streams.Streams()
@@ -45,21 +48,35 @@ class Interface:
 
         return frame
 
+    @dask.delayed
+    def __get_specifications(self, code: str) -> pd.Series:
+        """
+
+        :param code:
+        :return:
+        """
+
+        return self.__reference.loc[
+               self.__reference['hospital_code'] == code, :].squeeze(axis=0)
+
     def exc(self):
         """
 
         :return:
         """
 
-        listings = glob.glob(
-            pathname=os.path.join(self.__configurations.data_, 'data', '**', 'features.csv'))
+        path = os.path.join(self.__configurations.data_, 'data', '{code}', 'features.csv')
 
+        # Identification Codes
+        codes = self.__reference['hospital_code'].unique()
 
+        # Compute
         computations = []
-        for listing in listings:
-            data = self.__get_data(uri=listing)
+        for code in codes:
+            data = self.__get_data(uri=path.format(code=code))
             data = self.__structuring(blob=data.copy())
-            message = self.__persist(data=data)
+            specifications = self.__get_specifications(code=code)
+            message = self.__persist(data=data, specifications=specifications)
             computations.append(message)
         messages = dask.compute(computations, scheduler='threads')[0]
 
