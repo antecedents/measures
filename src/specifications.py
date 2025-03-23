87fee6a5668c169c1707d3e86b68ed5d8b2a14fb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-23 14:23:26 +0000

    Update src/specifications.py

--- src/specifications.py
+++ src/specifications.py
@@ -1,6 +1,14 @@
 
+import pandas as pd
+
 
 class Specifications:
 
-    def __init__(self):
-        pass
+    def __init__(self, reference: pd.DataFrame):
+
+        self.__reference = reference
+
+    def __call__(self, code: str):
+
+        return self.__reference.loc[
+               self.__reference['hospital_code'] == code, :].squeeze(axis=0)
