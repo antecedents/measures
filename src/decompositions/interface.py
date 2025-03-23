5a356e332d4e18e19ad298d3c8c74152f2da6ad1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-23 14:55:08 +0000

    Update src/decompositions/interface.py

--- src/decompositions/interface.py
+++ src/decompositions/interface.py
@@ -11,6 +11,7 @@ import src.decompositions.persist
 import src.decompositions.structuring
 import src.elements.text_attributes as txa
 import src.functions.streams
+import src.specifications
 
 
 class Interface:
@@ -18,13 +19,10 @@ class Interface:
     The interface to the programs that prepare the appropriate data structures for graphing the decompositions.
     """
 
-    def __init__(self, reference: pd.DataFrame):
+    def __init__(self):
         """
 
-        :param reference:
         """
-
-        self.__reference = reference
 
         self.__configurations = config.Config()
         self.__streams = src.functions.streams.Streams()
@@ -48,34 +46,27 @@ class Interface:
 
         return frame
 
-    @dask.delayed
-    def __get_specifications(self, code: str) -> pd.Series:
+    def exc(self, reference: pd.DataFrame):
         """
 
-        :param code:
-        :return:
-        """
-
-        return self.__reference.loc[
-               self.__reference['hospital_code'] == code, :].squeeze(axis=0)
-
-    def exc(self):
-        """
-
+        :param reference:
         :return:
         """
 
         path = os.path.join(self.__configurations.data_, 'data', '{code}', 'features.csv')
 
         # Identification Codes
-        codes = self.__reference['hospital_code'].unique()
+        codes = reference['hospital_code'].unique()
+
+        # Specifications
+        specifications = dask.delayed(src.specifications.Specifications(reference=reference))
 
         # Compute
         computations = []
         for code in codes:
+            specifications = specifications(code=code)
             data = self.__get_data(uri=path.format(code=code))
             data = self.__structuring(blob=data.copy())
-            specifications = self.__get_specifications(code=code)
             message = self.__persist(data=data, specifications=specifications)
             computations.append(message)
         messages = dask.compute(computations, scheduler='threads')[0]
