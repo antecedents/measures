bad567996802b6e6aa73550dcb6fcbefcc07e932
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-23 14:25:02 +0000

    Update src/specifications.py

--- src/specifications.py
+++ src/specifications.py
@@ -1,14 +1,29 @@
-
+"""Module specifications.py"""
 import pandas as pd
 
 
 class Specifications:
+    """
+    <b>Notes</b><br>
+    ------<br>
+
+    For extracting details by institution
+    """
 
     def __init__(self, reference: pd.DataFrame):
+        """
+
+        :param reference:
+        """
 
         self.__reference = reference
 
     def __call__(self, code: str):
+        """
+
+        :param code:
+        :return:
+        """
 
         return self.__reference.loc[
                self.__reference['hospital_code'] == code, :].squeeze(axis=0)
