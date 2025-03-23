0537060a50b278f56ca58f6f8318a054f84ba969
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-23 17:07:27 +0000

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -8,8 +8,6 @@ import src.data.codes
 import src.data.menu
 import src.data.reference
 import src.elements.s3_parameters as s3p
-import src.s3.directives
-import src.s3.unload
 
 
 class Interface:
@@ -32,33 +30,12 @@ class Interface:
 
         # Setting up
         self.__configurations = config.Config()
-        self.__source_bucket = self.__s3_parameters.internal
-        self.__prefix = self.__s3_parameters.path_internal_artefacts + self.__configurations.stamp
-
-        # Directives
-        self.__directives = src.s3.directives.Directives()
-
-    def __get_assets(self) -> int:
-        """
-
-        :return:
-        """
-
-        try:
-            return self.__directives.synchronise(
-                source_bucket=self.__source_bucket, origin=self.__prefix, target=self.__configurations.data_)
-        except RuntimeError as err:
-            raise err from err
 
     def exc(self) -> pd.DataFrame:
         """
 
         :return:
         """
-
-
-        state = self.__get_assets()
-        logging.info(state)
 
         codes: list[str] = src.data.codes.Codes().exc()
         reference = src.data.reference.Reference(
