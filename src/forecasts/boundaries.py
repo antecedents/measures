c4625724751fbdfcc39c49367513286212f5c707
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-26 23:14:11 +0000

    Update src/forecasts/boundaries.py

---
+++ src/forecasts/boundaries.py
@@ -0,0 +1,14 @@
+
+import src.elements.parts as pr
+
+class Boundaries:
+
+    def __init__(self):
+        pass
+
+    def exc(self, parts: pr.Parts):
+        """
+
+        :param parts:
+        :return:
+        """
