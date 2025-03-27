2c2c57ccd0eb241ec68195e8d39418bd8752bfbe
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-03-27 16:18:00 +0000

    Update src/forecasts/parts.py

--- src/forecasts/parts.py
+++ src/forecasts/parts.py
@@ -1,5 +1,6 @@
 """Module parts.py"""
 import typing
+import os
 
 import numpy as np
 import pandas as pd
@@ -50,24 +51,36 @@ class Parts:
                 tests[self.__fields].rename(columns=self.__rename),
                 futures[self.__fields].rename(columns=self.__rename))
 
-    def __append_features(self, uri: str):
+    def __append_features(self, estimates: pd.DataFrame, uri: str) -> pd.DataFrame:
         """
 
+        :param estimates:
         :param uri:
         :return:
         """
 
-        text = txa.TextAttributes(uri=uri, header=0)
-        self.__streams.read(text=text)
+        fields = ['trend', 'residue', 'seasonal']
 
-    def exc(self, seasonal: sa.Seasonal, trend: pd.DataFrame) -> pr.Parts:
+        # Reading-in the features data
+        text = txa.TextAttributes(uri=uri, header=0)
+        features = self.__streams.read(text=text)
+
+        # Merging ...
+        return estimates.merge(features[fields], how='left', on='week_ending_date')
+
+    def exc(self, seasonal: sa.Seasonal, trend: pd.DataFrame, code: str) -> pr.Parts:
         """
 
         :param seasonal: The seasonal components estimations.
         :param trend: The trend components estimations.
+        :param code: The identification code of an institution/hospital.
         :return:
         """
 
         estimates, tests, futures = self.__get_parts(seasonal=seasonal, trend=trend)
+
+        # Extending estimates
+        uri = os.path.join(self.__configurations.data_, 'data', code, 'features.csv')
+        estimates = self.__append_features(estimates=estimates.copy(), uri=uri)
 
         return pr.Parts(estimates=estimates, tests=tests, futures=futures)
