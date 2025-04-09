f6eda17ea3eea90b7ab4ebe4291d59b2391bb1db
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-09 12:19:29 +0100

    Add src/data/specifications.py

---
+++ src/data/specifications.py
@@ -0,0 +1,26 @@
+"""Module specifications.py"""
+import pandas as pd
+
+import src.elements.specifications as se
+
+
+class Specifications:
+    """
+    Creates an attributes collection per gauge
+    """
+
+    def __init__(self):
+        pass
+
+    def exc(self, reference: pd.DataFrame) -> list[se.Specifications]:
+        """
+
+        :param reference:
+        :return:
+        """
+
+        dictionaries = [reference.iloc[i, :].squeeze() for i in range(reference.shape[0])]
+
+        specifications_ = [se.Specifications(**dictionary) for dictionary in dictionaries]
+
+        return specifications_
