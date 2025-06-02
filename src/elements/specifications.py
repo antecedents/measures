3848fad02ec2fb50fee07b92e5bf5db34f983675
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-02 21:35:32 +0100

    Update src/elements/specifications.py

--- src/elements/specifications.py
+++ src/elements/specifications.py
@@ -3,21 +3,37 @@ import typing
 
 class Specifications(typing.NamedTuple):
     """
-    The data type class ⇾ Seasonal
+    The data type class ⇾ Specifications
 
     Attributes
     ----------
-    health_board_code :
-        A health board code
-    health_board_name:
-        The health board name
+
     hospital_code:
         A hospital code
     hospital_name:
         The hospital's name
+    health_board_code :
+        A health board code
+    health_board_name:
+        The health board name
+    post_code:
+        Post Code
+    hscp_code:
+        HSCP
+    council_area:
+        Council Area
+    intermediate_zone:
+        Intermediate Zone
+    data_zone:
+        Data Zone
     """
 
-    health_board_code: str
-    health_board_name: str
     hospital_code: str
     hospital_name: str
+    health_board_code: str
+    health_board_name: str
+    post_code: str
+    hscp_code: str
+    council_area: str
+    intermediate_zone: str
+    data_zone: str
