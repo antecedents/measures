b0242450a48010ad5c1be53115436c37a5571839
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-02 22:09:33 +0100

    Update src/preface/interface.py

--- src/preface/interface.py
+++ src/preface/interface.py
@@ -1,6 +1,5 @@
 """Module interface.py"""
 import logging
-import sys
 import typing
 
 import boto3
@@ -8,11 +7,9 @@ import boto3
 import config
 import src.elements.s3_parameters as s3p
 import src.elements.service as sr
-import src.functions.cache
 import src.functions.service
 import src.preface.setup
 import src.s3.configurations
-import src.s3.keys
 import src.s3.s3_parameters
 
 
@@ -22,34 +19,24 @@ class Interface:
     """
 
     def __init__(self):
-        pass
+        """
+        Constructor
+        """
 
-    @staticmethod
-    def __get_arguments(connector: boto3.session.Session) -> dict:
+        self.__configurations = config.Config()
+
+    def __get_arguments(self, connector: boto3.session.Session) -> dict:
         """
 
         :return:
         """
 
-        key_name = 'artefacts' + '/' + 'architecture' + '/' + 'arguments.json'
+        key_name = self.__configurations.arguments_key
 
-        return src.s3.configurations.Configurations(connector=connector).objects(key_name=key_name)
+        arguments =  src.s3.configurations.Configurations(connector=connector).objects(key_name=key_name)
+        logging.info(arguments)
 
-    @staticmethod
-    def __proceed(service: sr.Service, bucket_name: str):
-        """
-
-        :param service:
-        :param bucket_name:
-        :return:
-        """
-
-        prefix = f'artefacts/{config.Config().stamp}'
-
-        value = src.s3.keys.Keys(service=service, bucket_name=bucket_name).excerpt(prefix=prefix)
-        logging.info('KEYS: %s', value)
-
-        return value
+        return arguments
 
     def exc(self) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
         """
@@ -61,12 +48,9 @@ class Interface:
         s3_parameters: s3p.S3Parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
         service: sr.Service = src.functions.service.Service(
             connector=connector, region_name=s3_parameters.region_name).exc()
-
-        if not self.__proceed(service=service, bucket_name=s3_parameters.internal):
-            src.functions.cache.Cache().exc()
-            sys.exit('EMPTY')
-
         arguments: dict = self.__get_arguments(connector=connector)
-        src.preface.setup.Setup().exc()
+
+        src.preface.setup.Setup(
+            service=service, s3_parameters=s3_parameters).exc()
 
         return connector, s3_parameters, service, arguments
