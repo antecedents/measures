bf2fa5b15cc2a37754f800f5b10232a730fa4933
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-02 23:14:22 +0100

    Update src/preface/interface.py

--- src/preface/interface.py
+++ src/preface/interface.py
@@ -1,10 +1,8 @@
 """Module interface.py"""
-import logging
 import typing
 
 import boto3
 
-import config
 import src.elements.s3_parameters as s3p
 import src.elements.service as sr
 import src.functions.service
@@ -19,11 +17,7 @@ class Interface:
     """
 
     def __init__(self):
-        """
-        Constructor
-        """
-
-        self.__configurations = config.Config()
+        pass
 
     @staticmethod
     def exc() -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service]:
@@ -36,7 +30,6 @@ class Interface:
         s3_parameters: s3p.S3Parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
         service: sr.Service = src.functions.service.Service(
             connector=connector, region_name=s3_parameters.region_name).exc()
-
 
         src.preface.setup.Setup(
             service=service, s3_parameters=s3_parameters).exc()
