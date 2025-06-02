1ce5b47bd9cd3438e3d0cf2fe97cd8abd9f5490f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-02 23:09:59 +0100

    Update src/preface/interface.py

--- src/preface/interface.py
+++ src/preface/interface.py
@@ -25,20 +25,8 @@ class Interface:
 
         self.__configurations = config.Config()
 
-    def __get_arguments(self, connector: boto3.session.Session) -> dict:
-        """
-
-        :return:
-        """
-
-        key_name = self.__configurations.arguments_key
-
-        arguments =  src.s3.configurations.Configurations(connector=connector).objects(key_name=key_name)
-        logging.info(arguments)
-
-        return arguments
-
-    def exc(self) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
+    @staticmethod
+    def exc() -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service]:
         """
 
         :return:
@@ -48,9 +36,9 @@ class Interface:
         s3_parameters: s3p.S3Parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
         service: sr.Service = src.functions.service.Service(
             connector=connector, region_name=s3_parameters.region_name).exc()
-        arguments: dict = self.__get_arguments(connector=connector)
+
 
         src.preface.setup.Setup(
             service=service, s3_parameters=s3_parameters).exc()
 
-        return connector, s3_parameters, service, arguments
+        return connector, s3_parameters, service
