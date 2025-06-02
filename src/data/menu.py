d7dd3836e8451d535f3fa3968001d99cb9fc6512
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-02 21:35:52 +0100

    Update src/data/menu.py

--- src/data/menu.py
+++ src/data/menu.py
@@ -23,15 +23,21 @@ class Menu:
     def exc(self, reference: pd.DataFrame):
         """
 
-        :param reference: The institutions/hospitals & health board reference.
+        :param reference: A reference sheet.
         :return:
         """
 
-        frame = pd.DataFrame(data={'desc': reference['hospital_code'].to_numpy(),
-                                   'name': reference['hospital_name'].to_numpy()})
+        # Sort
+        excerpt = reference.copy().sort_values(by=['health_board_name', 'hospital_name'], ascending=True)
 
+        # Nodes
+        names = (excerpt['hospital_name'] + '/' + excerpt['health_board_name']).to_numpy()
+        frame = pd.DataFrame(data={'desc': excerpt['health_board_code'].to_numpy(),
+                                   'name': names})
         nodes = frame.to_dict(orient='records')
+        logging.info(nodes)
 
+        # Persist
         message = src.functions.objects.Objects().write(
             nodes=nodes, path=os.path.join(self.__configurations.menu_, 'menu.json'))
         logging.info('Graphing Menu ->\n%s', message)
